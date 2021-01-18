152f94ab7e327cbf4c4a6b81281b7aab5e9a8a0f
Matthias Bernt <m.bernt@ufz.de>
2021-01-18 14:59:39 +0100

    cnt

--- planemo_ci_actions.sh
+++ planemo_ci_actions.sh
@@ -129,7 +129,7 @@ fi
 
 if [ "$PLANEMO_COMBINE_OUTPUTS" == "true" ]; then
   ls -R artifacts/ 
-  find artifacts/ -name tool_test_output.json -exec sh -c 'echo "$@"' sh {} +
+  find -D exec artifacts/ -name tool_test_output.json -exec sh -c 'echo planemo merge_test_reports "$@" upload/tool_test_output.json' sh {} +
   find -D exec artifacts/ -name tool_test_output.json -exec sh -c 'planemo merge_test_reports "$@" upload/tool_test_output.json' sh {} +
   cat upload/tool_test_output.json
   [ "$PLANEMO_HTML_REPORT" == "true" ] && planemo test_reports upload/tool_test_output.json --test_output upload/tool_test_output.html
